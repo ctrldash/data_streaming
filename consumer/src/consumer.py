2c4090bd776b7be79f552af9036418ca46e2a3be
Vadym <popikvi@gmail.com>
2023-06-09 23:13:32 +0300

    Consumer first step

--- consumer/src/consumer.py
+++ consumer/src/consumer.py
@@ -2,15 +2,16 @@ import sys
 from confluent_kafka import Consumer, KafkaException, KafkaError
 
 
-def msg_process(msg, out):
-    message = msg.split(",")
-    print(message[0], file=out)
+def msg_process(msg):
+    print("I`m here, inside msg_process function")
+    print(msg.value())
+    print(msg.topic())
 
 
 def basic_consume_loop(consumer, topics):
     try:
         consumer.subscribe(topics)
-        out = open("./src/results.csv", "a")
+        # out = open("./src/results.csv", "a")
 
         while running:
             msg = consumer.poll(timeout=1.0)
@@ -29,11 +30,13 @@ def basic_consume_loop(consumer, topics):
         # Close down consumer to commit final offsets.
         consumer.close()
 
+
 def shutdown():
     running = False
 
+
 if __name__ == '__main__':
-    conf = {'bootstrap.servers': 'kafka-1:9092,kafka-2:9092,kafka-3:9092',
+    conf = {'bootstrap.servers': 'kafka-1:9092',
         'group.id': "super3",
         'enable.auto.commit': False,
         'auto.offset.reset': 'earliest'}
